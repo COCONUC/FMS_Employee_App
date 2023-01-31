36a65383689c1fed0c3fca0103a2ea79557823ba
Sang Nguyen Tan <sos.coconut@gmail.com>
2023-02-01 05:35:34 +0700

    Update verify_dialog.dart

--- lib/widgets/dialog/verify_dialog.dart
+++ lib/widgets/dialog/verify_dialog.dart
@@ -1,57 +1,84 @@
+import 'package:flutter/material.dart';
 import 'package:fms_employee/constants/color_constant.dart';
 import 'package:fms_employee/constants/constant.dart';
 import 'package:fms_employee/constants/resizer/fetch_pixels.dart';
 import 'package:fms_employee/constants/widget_utils.dart';
-import 'package:flutter/material.dart';
+import 'package:fms_employee/models/order_detail_data.dart';
 
-
-class VerifyDialog extends StatefulWidget {
-  const VerifyDialog({Key? key}) : super(key: key);
+class OptionDialog extends StatefulWidget {
+  final String title;
+  final String message;
+  const OptionDialog(this.title, this.message, {Key? key}) : super(key: key);
 
   @override
-  State<VerifyDialog> createState() => _VerifyDialogState();
+  State<OptionDialog> createState() => _OptionDialogState();
 }
 
-class _VerifyDialogState extends State<VerifyDialog> {
+class _OptionDialogState extends State<OptionDialog> {
+  Future<bool> _onWillPop() async {
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     FetchPixels(context);
-    return AlertDialog(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(FetchPixels.getPixelHeight(20))),
-      backgroundColor: backGroundColor,
-      content: Builder(
-        builder: (context) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              getVerSpace(FetchPixels.getPixelHeight(20)),
-              getSvgImage("group.svg"),
-              getVerSpace(FetchPixels.getPixelHeight(30)),
-              getCustomFont("Account Created", 22, Colors.black, 1,
-                   fontWeight: FontWeight.w900),
-              getVerSpace(FetchPixels.getPixelHeight(10)),
-              getMultilineCustomFont(
-                  "Your account has been successfully created!",
-                  16,
-                  Colors.black,
-                  fontWeight: FontWeight.w400,
-                  
-                  textAlign: TextAlign.center,
-                  txtHeight:1.3),
-              getVerSpace(FetchPixels.getPixelHeight(30)),
-              getButton(context, blueColor, "Ok", Colors.white, () {
-                /*Constant.sendToNext(context, Routes.loginRoute);*/
-              }, 18,
-                  weight: FontWeight.w600,
-                  buttonHeight: FetchPixels.getPixelHeight(60),
-                  borderRadius:
-                      BorderRadius.circular(FetchPixels.getPixelHeight(15))),
-              getVerSpace(FetchPixels.getPixelHeight(20)),
-            ],
-          );
-        },
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: AlertDialog(
+        shape: RoundedRectangleBorder(
+            borderRadius:
+            BorderRadius.circular(FetchPixels.getPixelHeight(20))),
+        backgroundColor: backGroundColor,
+        content: Builder(
+          builder: (context) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                /*getVerSpace(FetchPixels.getPixelHeight(20)),*/
+                /* getSvgImage("confirm.svg",
+                    width: FetchPixels.getPixelHeight(71.37),
+                    height: FetchPixels.getPixelHeight(99.92)),*/
+                getVerSpace(FetchPixels.getPixelHeight(20)),
+                getCustomFont(widget.title, 22, Colors.black, 1,
+                  fontWeight: FontWeight.w900, ),
+                getVerSpace(FetchPixels.getPixelHeight(10)),
+                getMultilineCustomFont(
+                    widget.message,
+                    16,
+                    Colors.black,
+
+                    fontWeight: FontWeight.w400,
+                    txtHeight:1.3,
+                    textAlign: TextAlign.center),
+                getVerSpace(FetchPixels.getPixelHeight(30)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    getButton(context, blueColor, "Xác nhận", Colors.white, () {
+
+                    }, 18,
+                        weight: FontWeight.w600,
+                        buttonHeight: FetchPixels.getPixelHeight(60),
+                        buttonWidth: FetchPixels.getPixelWidth(100),
+                        borderRadius:
+                        BorderRadius.circular(FetchPixels.getPixelHeight(14))),
+                    getHorSpace(FetchPixels.getPixelWidth(20)),
+                    getButton(context, blueColor, "Quay lại", Colors.white, () {
+                      Constant.backToPrev(context);
+                    }, 18,
+                        weight: FontWeight.w600,
+                        buttonHeight: FetchPixels.getPixelHeight(60),
+                        buttonWidth: FetchPixels.getPixelWidth(100),
+                        borderRadius:
+                        BorderRadius.circular(FetchPixels.getPixelHeight(14))),
+                  ],
+                ),
+                getVerSpace(FetchPixels.getPixelHeight(10)),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
